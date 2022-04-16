e62b78861288ae5152ea2796b6b2bc1e7fbbcb70
DedsecDissident <dedsecdissident@gmail.com>
2022-04-16 12:37:02 +0300

    Выполнено Задание 1.1

--- src/Tasks1/Main.java
+++ src/Tasks1/Main.java
@@ -1,7 +1,10 @@
 package Tasks1;
 
+import Tasks1.Exercise_1.Easy_Search;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Easy_Search easySearch = new Easy_Search();
+        easySearch.method();
     }
 }
